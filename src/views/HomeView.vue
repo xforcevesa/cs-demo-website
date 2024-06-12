3f616854f0f337e711f8ef4a7c7556b7bfbac663
xforcevesa <nomodeset@qq.com>
2024-06-12 19:35:48 +0800

    Add NavBar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Welcome to the Demo Website</h1>
   </main>
 </template>
